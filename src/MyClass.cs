1975354d4825019c1966d6b886fcfda53b8926b3
Kenneth Vedder <kennethvedder@live.com>
2025-06-23 00:30:15 -0500

    eighth, newer test

---
+++ src/MyClass.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+
+public class MyClass : MonoBehaviour
+{
+	public float x;
+	public int y;
+	
+	void Start()
+	{
+		x = 34;
+		y = 12;
+		
+		print(x);
+	}
+}
